881f47a823679f43f59dc9ce8ddd62c6a3df33b1
Louka Morneau <morneaulouka@hotmail.com>
2019-02-23 02:31:28 -0500

    added token finder (Need better "declaration" action)
    Next step is to look for a better way to implement the declaration token to catch both dec id; and dec id = value; Same with for loop token

--- TP1CompilLexer/TP1CompilLexer/TP1CompilLexer.cpp
+++ TP1CompilLexer/TP1CompilLexer/TP1CompilLexer.cpp
@@ -3,8 +3,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <regex>
 
 using namespace std;
+
+vector<string> m_declarationType = { "int", "float", "bool", "char", "string" };
+regex m_identificatorRegex("^[a-zA-Z][a-zA-Z0-9_]*[a-zA-Z]$|^[a-zA-Z]$"); //Commence par une lettre, nimportequoi dans le millieu et fini par une lettre OU est une lettre
+vector<string> m_conditionType = { "if" };
+vector<string> m_operateurType = { "==", "!=", "<", ">" };
+vector<string> m_loopType = { "for" };
+
+vector<string> *m_listTokens = new vector<string>();
+
+vector<void> *m_listIds = new vector<void>();
 
 bool readFile(string fileToRead, string* fileContent)
 {
@@ -21,31 +34,84 @@ bool readFile(string fileToRead, string* fileContent)
 	return true;
 }
 
-bool lexer(string* fileContent)
+int lexer(string* fileContent)
 {
 	int i = 0;
-	bool error = false;
+	string lex = "";
 
-	while (i < fileContent->length() && !error)
+	while (i < fileContent->length())
 	{
-		if (true)
+		if (fileContent->at(i) == ' ' || fileContent->at(i) == ';' || fileContent->at(i) == '\n')//reset lexem
 		{
+			//check to see what lexem looks like
+			if (find(m_declarationType.begin(), m_declarationType.end(), lex) != m_declarationType.end())//Lexem is a declaration
+			{
+				m_listTokens->push_back("DEC: " + lex);
+				//dec id;
+				//OR
+				//dec id = ...;
+			}
+			else
+			{
+				if (find(m_conditionType.begin(), m_conditionType.end(), lex) != m_conditionType.end())//Lexem is a condition
+				{
+					m_listTokens->push_back("COND: " + lex);
+				}
+				else
+				{
+					if (find(m_operateurType.begin(), m_operateurType.end(), lex) != m_operateurType.end())//Lexem is an operator
+					{
+						m_listTokens->push_back("OP: " + lex);
+					}
+					else
+					{
+						if (find(m_loopType.begin(), m_loopType.end(), lex) != m_loopType.end())//Lexem is a loop
+						{
+							m_listTokens->push_back("LOOP: " + lex);
+						}
+						else
+						{
+							smatch match;
+							if (regex_match(lex, match, m_identificatorRegex))//Lexem match identificator
+							{
+								m_listTokens->push_back("ID: " + lex);
+							}
+							else
+							{
+								m_listTokens->push_back("ERROR: " + lex);
+							}
+						}
+					}
+				}
+			}
 
+			//then reset lexem
+			lex = "";
+		}
+		else
+		{
+			lex += fileContent->at(i);
 		}
 		i++;
 	}
-	return error;
+	return 0;
 }
 
 int main()
 {
 	string fileContent = "";
 
-	readFile("exemple.cs", &fileContent);
+	readFile("example.cs", &fileContent);
 
 	lexer(&fileContent);
 
-    cout << fileContent;
+	cout << fileContent << endl;
+
+	for (int i = 0; i < m_listTokens->size(); i++)
+	{
+		cout << m_listTokens->at(i) << endl;
+	}
+
 	int i;
 	cin >> i;
 }
