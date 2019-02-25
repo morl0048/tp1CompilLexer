d933f35d8db04289d9b3a47c50d8f8c235d582ba
Louka Morneau <morneaulouka@hotmail.com>
2019-02-24 23:55:54 -0500

    end me

--- TP1CompilLexer/TP1CompilLexer/TP1CompilLexer.cpp
+++ TP1CompilLexer/TP1CompilLexer/TP1CompilLexer.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-regex m_regexKeyword("^[\-\!\$\%\^\&\*\(\)\_\+\|\~\=\`\{\}\\\[\\\]\:\"\;\'\<\>\?\,\.\\\/]$"); //One of the folowing:
+//regex m_regexKeyword("^[\-\!\$\%\^\&\*\(\)\_\+\|\~\=\`\{\}\\\[\\\]\:\"\;\'\<\>\?\,\.\\\/]$"); //One of the folowing:
 regex m_regexType("^(int|float|bool|char|string)$"); //One of the following: "int","float","bool","char","string"
 regex m_regexInt("^[+-]?[1-9][0-9]*$|^0$"); //-(OPTIONNAL) 1-9 as first digit followed by any number of digit OR 0
 regex m_regexFloat("^[+-]?[0-9]+(\.[0-9]+)?$"); //-(OPTIONNAL) 0-9 at least one time + (. 0-9 at least one time (OPTIONNAL))
@@ -17,33 +17,20 @@ regex m_regexBool("^(0|1|true|false)$"); //0 or 1 or true or flase
 regex m_regexChar("^\'.\'$"); //One char between ''
 regex m_regexString("^\".*\"$"); //Any number of char between ""
 
-regex m_regexCondition("^if$"); //Starts with if( + contains anything + ends with ) + may be followed by { + contains anything + } //\(.*\)(\{.*\})?
-regex m_regexLeftP("^\($"); //Left parenthesis (
-regex m_regexRightP("^\)$"); //Right parenthesis )
-regex m_regexLeftB("^\{$"); //Left bracket (
-regex m_regexRightB("^\}$"); //Right braket )
+regex m_regexCondition("^if$"); //Starts with if
+//regex m_regexLeftP("^\($"); //Left parenthesis (
+//regex m_regexRightP("^\)$"); //Right parenthesis )
+//regex m_regexLeftC("^\{$"); //Left curly brace {
+//regex m_regexRightC("^\}$"); //Right curly brace }
+//regex m_regexLeftB("^\[$"); //Left bracket [
+//regex m_regexRightB("^\]$"); //Right bracket ]
 
+regex m_regexOperator("^[==,!=,<,>,+,-]$"); //One of the following: "==","!=","<",">","+","-"
+
+regex m_regexLoop("^for$"); //Starts with for
 
 regex m_regexIdentificator("^[a-zA-Z][a-zA-Z0-9_$]*[a-zA-Z]$|^[a-zA-Z]$"); //Starts with a letter + any char in between + ends with a letter OR is a letter
 
-
-//regex m_regexOperator("^[==,!=,<,>]$"); //One of the following: "==","!=","<",">"
-
-//regex m_regexLoop("^for\(_*;_*;_*\)?(\{_*\})$"); //Starts with for( + contains anything + ; + contains anything + ; + contains anything + ends with ) + may be followed by { + contains anything + }
-
-
-//regex m_declarationType("int ([a-zA-Z][a-zA-Z0-9_]*[a-zA-Z]|[a-zA-Z]);");
-//regex m_identificatorRegex("[a-zA-Z][a-zA-Z0-9_]*[a-zA-Z]|[a-zA-Z]"); //Commence par une lettre, nimportequoi dans le millieu et fini par une lettre OU est une lettre
-//regex m_conditionRegex("");
-//regex m_operatorType("[==,!=,<,>]");
-//regex m_loopRegex("for(;;)");
-//
-//vector<string> m_declarationType = { "int", "float", "bool", "char", "string" };
-//
-//vector<string> m_conditionType = { "if" };
-//vector<string> m_operateurType = { "==", "!=", "<", ">" };
-//vector<string> m_loopType = { "for" };
-//
 vector<string> *m_tokenTable = new vector<string>();
 
 bool readFile(string fileToRead, string* fileContent)
@@ -61,112 +48,6 @@ bool readFile(string fileToRead, string* fileContent)
 	return true;
 }
 
-//int lexer(string* fileContent)
-//{
-//	int i = 0;
-//	string lex = "";
-//
-//	while (i < fileContent->length())
-//	{
-//		if (fileContent->at(i) == ' ' || fileContent->at(i) == ';' || fileContent->at(i) == '\n')//reset lexem
-//		{
-//			//check to see what lexem looks like
-//			if (find(m_declarationType.begin(), m_declarationType.end(), lex) != m_declarationType.end())//Lexem is a declaration
-//			{
-//				string decType = lex;
-//				lex = "";
-//
-//				//dec id;
-//				//OR
-//				//dec id = ...;
-//				//OR
-//				//dec id0, id1 =..., id2;
-//				while (fileContent->at(i) != ';')
-//				{
-//					if (fileContent->at(i) == ',')//keep declaration and reset a new one
-//					{
-//						m_lexTable->push_back("DEC: " + decType + " " + lex);
-//						lex = "";
-//					}
-//					else
-//					{
-//						if (fileContent->at(i) != ' ')
-//						{
-//							lex += fileContent->at(i);
-//						}
-//					}
-//
-//					i++;
-//				}
-//				m_lexTable->push_back("DEC: " + decType + " " + lex);
-//			}
-//			else
-//			{
-//				if (find(m_conditionType.begin(), m_conditionType.end(), lex) != m_conditionType.end())//Lexem is a condition
-//				{
-//					string condType = lex;
-//					lex = "";
-//					i++;//next char == (
-//					if (fileContent->at(i) != '(')//read all inside if()
-//					{
-//						while (fileContent->at(i) != ')' && i < fileContent->length())
-//						{
-//							lex += fileContent->at(i);
-//							i++;
-//						}
-//						if (i == fileContent->length())
-//						{
-//							m_lexTable->push_back("ERROR: " + lex);
-//						}
-//						else
-//						{
-//						}
-//						
-//					}
-//					else
-//					{
-//
-//					}
-//
-//					
-//					//if (find(m_operateurType.begin(), m_operateurType.end(), lex) != m_operateurType.end())//Lexem is an operator
-//					//{
-//					//	m_lexTable->push_back("OP: " + lex);
-//					//}
-//				}
-//				else
-//				{
-//					if (find(m_loopType.begin(), m_loopType.end(), lex) != m_loopType.end())//Lexem is a loop
-//					{
-//						m_lexTable->push_back("LOOP: " + lex);
-//					}
-//					else
-//					{
-//						smatch match;
-//						if (regex_match(lex, match, m_identificatorRegex))//Lexem match identificator
-//						{
-//							m_lexTable->push_back("ID: " + lex);
-//						}
-//						else
-//						{
-//							m_lexTable->push_back("ERROR: " + lex);
-//						}
-//					}
-//				}
-//			}
-//
-//			//then reset lexem
-//			lex = "";
-//		}
-//		else
-//		{
-//			lex += fileContent->at(i);
-//		}
-//		i++;
-//	}
-//	return 0;
-//}
-
 
 int lexer(string* fileContent)
 {
@@ -176,15 +57,40 @@ int lexer(string* fileContent)
 
 	while (i < fileContent->length())
 	{
-		if (fileContent->at(i) != ' ' && fileContent->at(i) != '\n' && fileContent->at(i) != ';')//remove spaces and line return from content
+		if (fileContent->at(i) != ' ' && fileContent->at(i) != '\t' && fileContent->at(i) != '\n' && fileContent->at(i) != ';' && fileContent->at(i) != '(' && fileContent->at(i) != ')'
+			&& fileContent->at(i) != '[' && fileContent->at(i) != ']' && fileContent->at(i) != '{' && fileContent->at(i) != '}')
 		{
 			lex += fileContent->at(i);
 		}
 		else//push token and reset lex
 		{
-			if (fileContent->at(i) == ';')//and that is not in for loop declaration
+			if (fileContent->at(i) == ';' || fileContent->at(i) == '(' || fileContent->at(i) == ')' || 
+				fileContent->at(i) == '[' || fileContent->at(i) == ']' || fileContent->at(i) == '{' || fileContent->at(i) == '}')
 			{
-				m_tokenTable->push_back("SEPARATOR: ;");
+				switch (fileContent->at(i))
+				{
+				case ';':
+					m_tokenTable->push_back("SEPARATOR: ;");
+					break;
+				case '(':
+					m_tokenTable->push_back("LEFTP: (");
+					break;
+				case ')':
+					m_tokenTable->push_back("RIGHTP: )");
+					break;
+				case '[':
+					m_tokenTable->push_back("LEFTB: [");
+					break;
+				case ']':
+					m_tokenTable->push_back("RIGHTB: ]");
+					break;
+				case '{':
+					m_tokenTable->push_back("LEFTC: {");
+					break;
+				case '}':
+					m_tokenTable->push_back("RIGHTC: }");
+					break;
+				}
 			}
 			else
 			{
@@ -227,13 +133,34 @@ int lexer(string* fileContent)
 										}
 										else
 										{
-											if (regex_match(lex, match, m_regexIdentificator))
+											if(regex_match(lex, match, m_regexCondition))
 											{
-												m_tokenTable->push_back("ID: " + lex);
+												m_tokenTable->push_back("COND: " + lex);
 											}
 											else
 											{
-												m_tokenTable->push_back("ERROR: " + lex);
+												if (regex_match(lex, match, m_regexLoop))
+												{
+													m_tokenTable->push_back("LOOP: " + lex);
+												}
+												else
+												{
+													if (regex_match(lex, match, m_regexOperator))
+													{
+														m_tokenTable->push_back("OP: " + lex);
+													}
+													else
+													{
+														if (regex_match(lex, match, m_regexIdentificator))
+														{
+															m_tokenTable->push_back("ID: " + lex);
+														}
+														else
+														{
+															m_tokenTable->push_back("ERROR: " + lex);
+														}
+													}
+												}
 											}
 										}
 									}
@@ -250,45 +177,6 @@ int lexer(string* fileContent)
 	}
 	return 0;
 }
-
-/*
-if (fileContent->at(i) != ';' && fileContent->at(i) != ' ')//keep everything in memory until you hit a semicolon ';' or a space ' '
-		{
-			//|| fileContent->at(i) != '\n' || fileContent->at(i) != ' '
-			lex += fileContent->at(i);
-		}
-		else//analyse what we have in memory
-		{
-			smatch matchType;
-			if (regex_match(lex, matchType, m_regexType))
-			{
-				string suite = "";
-				while (fileContent->at(i) != ';')
-				{
-					i++;
-					if (fileContent->at(i) == ' ')
-					{
-						smatch matchIdentificator;
-						if()
-					}
-					else
-					{
-						if (fileContent->at(i) == ',')
-						{
-							//push + reset suite
-							suite = "";
-						}
-						else
-						{
-							suite += fileContent->at(i);
-						}
-					}
-
-				}
-			}
-			lex = "";
-		}
-*/
 
 int main()
 {
--- TP1CompilLexer/TP1CompilLexer/example.cs
+++ TP1CompilLexer/TP1CompilLexer/example.cs
@@ -1,3 +1,10 @@
+for ( int i = 0 ; i < 10 ; i = i + 1 )
+{
+}
+if ( 10 > 15 ) 
+{
+	string a = "blabla" ;
+}
 10 ;
 10.01 ;
 true ;
